6e0cf9c63c930c48bc4ad11906f785f4caf1016c
GravityIO <Harryioannoucy@gmail.com>
2026-02-21 17:13:52 +0200

    github actions test

--- smopper.lua
+++ smopper.lua
@@ -1,4 +1,38 @@
-local BASE_URL = "https://raw.githubusercontent.com/GravityCY/CCC/refs/heads/master/lib"
+local AutoPrograms = {
+    BASE_URL = "https://raw.githubusercontent.com/GravityCY/CCC/refs/heads/master"
+}
+
+function AutoPrograms.fetch(file, place)
+    if (fs.exists(file)) then return end
+    local url = AutoPrograms.BASE_URL .. file
+
+    if (not http) then
+        error("HTTP is disabled in ComputerCraft config!")
+    end
+
+    local response = http.get(url)
+    if (not response) then
+        error("Failed to download module from: " .. url)
+    end
+
+    local content = response.readAll()
+    response.close()
+
+    -- Save file locally
+    local dir = fs.getDir(file)
+    if (dir ~= "" and not fs.exists(dir)) then
+        fs.makeDir(dir)
+    end
+
+    local f = fs.open(file, "w")
+    f.write(content)
+    f.close()
+
+    print("Downloaded '" .. fs.getName(file) .. "' successfully.")
+end
+
+package.loaded["AutoPrograms"] = AutoPrograms;
+
 local oldRequire = require
 
 function require(name)
