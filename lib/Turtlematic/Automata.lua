01b831ba53e4c7d661f8ef872d804fb9a2fc3236
GravityIO <Harryioannoucy@gmail.com>
2026-03-16 09:25:57 +0200

    automata bugs (classic)

--- lib/Turtlematic/Automata.lua
+++ lib/Turtlematic/Automata.lua
@@ -1,43 +1,10 @@
-local Peripheral = require("lib.Peripheral");
+local AutomataLib = {};
+---@class Automata : turtlematic.AutomataInstance
+local AutomataInstance = {};
 
-local autoPeriph = Peripheral.firstByPredicate(function(p) return p.getCooldown ~= nil end);
+local argFormatterMap = {}
 
-if (autoPeriph == nil) then return nil; end
-
-local Automata = {};
-
----@param fn function
----@param op string
----@param ... any
----@return any
-local function wait(fn, op, ...)
-    local cooldown = autoPeriph.getCooldown(op);
-    if (cooldown ~= nil and cooldown > 0) then
-        sleep(cooldown / 1000);
-    end
-    return fn(...);
-end
-
----@param methodName string
----@param formatter fun(...): op: string, table
-local function makeBlocking(methodName, formatter)
-    local method = autoPeriph[methodName];
-
-    return function(...)
-        local op = methodName
-        local args = {...}
-
-        if (formatter ~= nil) then
-            op, args = formatter(...)
-        end
-
-        return wait(method, op, table.unpack(args))
-    end
-end
-
-local formatterMap = {}
-
-function formatterMap.scan(scanType, ...)
+function argFormatterMap.scan(scanType, ...)
     local operation;
     if (scanType == "item") then
         operation = "scanItems";
@@ -52,25 +19,87 @@ function formatterMap.scan(scanType, ...)
     return operation, {...};
 end
 
-for methodName, method in pairs(autoPeriph) do
-    if (type(method) ~= "function") then goto continue; end
+---@param self Automata
+---@param fn function
+---@param op string
+---@param ... any
+---@return ...
+local function wait(self, fn, op, ...)
+    local cooldown = self.automata.getCooldown(op);
+    if (cooldown ~= nil and cooldown > 0) then
+        sleep(cooldown / 1000);
+    end
+    return fn(...);
+end
 
-    local formatter = formatterMap[methodName];
-    if (formatter) then
-        Automata[methodName] = makeBlocking(methodName, formatterMap[methodName]);
+---@param self Automata
+---@param methodName string
+---@param formatter? fun(...): string, table
+local function makeBlocking(self, methodName, formatter)
+    local og = self.automata[methodName];
+
+    return function(_, ...)
+        local op = methodName
+        local args = {...}
+
+        if (formatter ~= nil) then
+            op, args = formatter(...)
+        end
+
+        return wait(self, og, op, table.unpack(args))
+    end
+end
+
+---@param self Automata
+---@param key string
+---@return any
+local function __index(self, key)
+    if (AutomataInstance[key] ~= nil) then return AutomataInstance[key]; end
+
+    local og = self.automata[key];
+    if (type(og) ~= "function") then return og; end
+
+    local methodName = key;
+
+    local formatter = argFormatterMap[methodName];
+    if (formatter ~= nil) then
+        self[methodName] = makeBlocking(self, methodName, formatter);
     else
-        local cooldown = autoPeriph.getCooldown(methodName);
+        local cooldown = self.automata.getCooldown(methodName);
         if (cooldown == nil) then
-            Automata[methodName] = method;
+            self[methodName] = function(_, ...) return og(...) end;
         else
-            Automata[methodName] = makeBlocking(methodName, formatterMap[methodName]);
+            self[methodName] = makeBlocking(self, methodName, nil);
         end
     end
-    ::continue::
+
+    return self[methodName];
 end
 
-function Automata.getConfiguration(arg)
-    return autoPeriph.getConfiguration()[arg];
+---@param obj turtlematic.Automata|string
+---@return Automata
+function AutomataLib.new(obj)
+    if (type(obj) == "string") then
+        ---@diagnostic disable-next-line: cast-local-type
+        obj = peripheral.wrap(obj);
+    end
+
+    if (obj == nil) then error("invalid automata") end
+
+    ---@class Automata
+    local self = {
+        ---@type turtlematic.Automata
+        ---@diagnostic disable-next-line: assign-type-mismatch
+        automata = obj;
+    };
+
+    return setmetatable(self, {__index = __index});
 end
 
-return Automata;
+function AutomataInstance:getConfiguration(arg)
+    if (arg == nil) then return self.automata.getConfiguration(); end
+
+    return self.automata.getConfiguration()[arg];
+end
+
+return AutomataLib;
