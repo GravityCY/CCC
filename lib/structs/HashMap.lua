15bdbda8667216edf74165ec2243501920d59d4d
GravityIO <Harryioannoucy@gmail.com>
2026-03-18 18:20:49 +0200

    hashmap class cause why not

--- lib/structs/HashMap.lua
+++ lib/structs/HashMap.lua
@@ -1,19 +1,27 @@
----@class HashMap<T, Y>
 local HashMap = {};
+---@class HashMap<T, Y>
+local HashMapInstance = {};
+HashMapInstance.__index = HashMapInstance;
+
+function HashMap.instanceof(obj)
+    return type(obj) == "table" and getmetatable(obj) == HashMapInstance;
+end
 
 function HashMap.new()
     ---@class HashMap<T, Y>
     local self = {
         data = {
-            ---@type table<T, Y>
             map = {};
+            size = 0;
         }
     };
 
-    return setmetatable(self, {__index=HashMap});
+    return setmetatable(self, HashMapInstance);
 end
 
-function HashMap:put(key, value)
+---@param key T
+---@param value Y
+function HashMapInstance:put(key, value)
     self.data.map[key] = value;
     if (value == nil) then
         self.data.size = self.data.size - 1;
@@ -22,27 +30,55 @@ function HashMap:put(key, value)
     end
 end
 
-function HashMap:get(key)
+---@param key T
+---@return Y
+function HashMapInstance:get(key)
     return self.data.map[key];
 end
 
-function HashMap:getOrCreate(key, value)
+---@param key T
+---@param value Y
+function HashMapInstance:getOrCreate(key, value)
     if (not self:exists(key)) then self:put(key, value); end
     return self.data.map[key];
 end
 
-function HashMap:exists(key)
+---@param key T
+---@return boolean
+function HashMapInstance:remove(key)
+    if (not self:exists(key)) then return false end
+
+    self.data.map[key] = nil;
+    self.data.size = self.data.size - 1;
+    return true;
+end
+
+---@param key T
+---@return boolean
+function HashMapInstance:exists(key)
     return self.data.map[key] ~= nil;
 end
 
-function HashMap:size()
+---@return integer
+function HashMapInstance:size()
     return self.data.size;
 end
 
-function HashMap:toArray()
+---@return KeyValue
+local function newPair(key, value)
+    ---@class KeyValue
+    local self = {
+        key = key,
+        value = value
+    };
+    return self;
+end
+
+---@return KeyValue[]
+function HashMapInstance:toArray()
     local arr = {};
     for key, value in pairs(self.data.map) do
-        arr[#arr + 1] = {key=key, value=value};
+        arr[#arr + 1] = newPair(key, value);
     end
     return arr;
 end
